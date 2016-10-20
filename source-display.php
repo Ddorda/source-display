8c283f4ebbc233596518103db76e1c808f6410c9
Ddorda <ddorda@ubuntu.com>
2016-10-20 13:20:44 +0300

    Making the code cleaner and clearer

--- source-display.php
+++ source-display.php
@@ -1,3 +1,36 @@
+<?php
+function highlight_num($file)
+{
+  for ($i = 1; $i <= count(file($file)); $i++) {
+        $lines = $lines."<a name='$i'>$i</a><br />";
+  }
+  $content = highlight_file($file, true);
+
+
+  echo ' 
+    <style type="text/css"> 
+        .num { 
+        float: left; 
+        color: gray; 
+        font-size: 13px;    
+        font-family: monospace; 
+        text-align: right; 
+        margin-right: 6pt; 
+        padding-right: 6pt; 
+        border-right: 1px solid gray;
+        line-height: 15px;}
+
+        body {margin: 0px; margin-left: 5px;} 
+        td {vertical-align: top;} 
+        code {white-space: nowrap;}
+        span {line-height: 15px;}
+    </style>';
+
+
+
+    echo "<table><tr><td class=\"num\">$lines</td><td>$content</td></tr></table>";
+}
+?>
 <html>
 <body>
 <?php
@@ -11,35 +44,3 @@ else {
 ?>
 </body>
 </html>
-
-<?php 
-function highlight_num($file) 
-{ 
-  for ($i = 1; $i <= count(file($file)); $i++) {
-	$lines = $lines."<a name='$i'>$i</a><br />"; // Create anchor for every line.
-  }
-  $content = highlight_file($file, true); 
-
-  
-  echo ' 
-    <style type="text/css"> 
-        .num { 
-        float: left; 
-        color: gray; 
-        font-size: 13px;    
-        font-family: monospace; 
-        text-align: right; 
-        margin-right: 6pt; 
-        padding-right: 6pt; 
-	line-height: 15px;
-        border-right: 1px solid gray;} 
-
-        body {margin: 0px; margin-left: 5px;} 
-        td {vertical-align: top;} 
-	span {line-height: 15px;}
-        code {white-space: nowrap;} 
-    </style>';
-    
-    echo "<table><tr><td class=\"num\">$lines</td><td>$content</td></tr></table>"; 
-} 
-?>
